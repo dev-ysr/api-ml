dcd331afe033a475932ad9f114e66bf8b655b75a
ysr_dev <yobanisolisramirez@outlook.com>
2021-07-01 18:13:36 -0500

    productos guardados en cache, se devuelve tanto del api de ML como de la cache

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const { Router } = require("express");
 const request = require("request");
 const router = Router();
+const { writeFile, readFile } = require("fs");
+let productsCache = require("../utils/storage.json");
+const { log } = require("console");
+const path = require("path");
 
 router.get("/api/search", (req, res) => {
     const query = req.query.query;
@@ -9,31 +13,36 @@ router.get("/api/search", (req, res) => {
         return;
     }
 
-    console.log(query);
+    if (!existsQuery(query)) {
 
-    const requestOptions = {
-        url: "https://api.mercadolibre.com/sites/MLA/search?q=" + query,
-        method: "GET",
-    };
+        const requestOptions = {
+            url: "https://api.mercadolibre.com/sites/MLA/search?q=" + query,
+            method: "GET",
+        };
 
-    request(requestOptions, (err, response, body) => {
-        if (err) {
-            console.log(err);
-        } else if (response.statusCode === 200) {
-            processResponse(req, res, body);
-        } else {
-            console.log(response.statusCode);
-        }
-    });
+        request(requestOptions, (err, response, body) => {
+            if (err) {
+                console.log(err);
+            } else if (response.statusCode === 200) {
+                processResponse(req, res, body, query);
+            } else {
+                console.log(response.statusCode);
+            }
+        });
+
+    } else {
+        res.json(productsCache[query]);
+    }
+
 
 });
 
-const processResponse = (req, res, responseBody) => {
+const processResponse = (req, res, responseBody, query) => {
     let data = JSON.parse(responseBody).results;
 
     let products = [];
 
-    data.forEach(item => {
+    data.forEach((item) => {
         product = {
             id: item.id,
             title: item.title,
@@ -41,16 +50,31 @@ const processResponse = (req, res, responseBody) => {
             currency_id: item.currency_id,
             available: item.available,
             thumbnail: item.thumbnail,
-            condition: item.condition
-        }
+            condition: item.condition,
+        };
         products.push(product);
     });
 
+    addToCache(query, products);
     res.json(products);
-}
+};
 
-const addToCache = (req, res, responseBody) => {
+const addToCache = (query, items) => {
+    productsCache[query] = items;
 
-}
+    const file = path.join(__dirname, "./../utils/storage.json");
+
+    writeFile(file, JSON.stringify(productsCache), () => {
+        console.log("write");
+    });
+};
+
+const existsQuery = (query) => {
+    if (productsCache[query]) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 module.exports = router;
